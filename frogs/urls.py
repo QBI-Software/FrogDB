b87725690e59b82c3e362739f164ef321bb3c78e
lizcw <e.cooperwilliams@uq.edu.au>
2016-07-25 13:26:22 +1000

    Redirect to Frogs after password change

--- frogs/urls.py
+++ frogs/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from django.conf import settings
-from django.contrib.auth.views import password_change,password_change_done,password_reset,password_reset_complete
-from django.contrib.auth.views import password_reset_confirm, password_reset_done
+from django.contrib.auth.views import password_change,password_change_done,password_reset,password_reset_complete, password_reset_confirm, password_reset_done
 from axes.decorators import watch_login
 from django.conf.urls.static import static
 from . import views
@@ -13,16 +12,12 @@ urlpatterns = [
     url(r'^login/$', watch_login(views.LoginView.as_view()), name='loginform'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^locked/$', views.locked_out, name='locked_out'),
-    url('^password_change/$', password_change, {'template_name': 'change-password.html'},name='password_change'),
-    url('^password_change/done/$', password_change_done, name='password_change_done'),
+    url('^password_change/$', password_change, {'template_name': 'admin/password_change_form.html', 'post_change_redirect': 'frogs:index'}, name='password_change'),
     url('^password_reset/$', password_reset, {'post_reset_redirect': 'frogs:index'},name='password_reset'),
     url('^password_reset/done/$', password_reset_done, name='password_reset_done'),
     url('^reset/(?P<uidb64>[0-9A-Za-z\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name':'admin/password_reset_email.html'}, name='password_reset_confirm'),
     url('^reset/done/$', password_reset_complete, {'template_name':'admin/password_reset_complete.html'}, name='password_reset_complete'),
-   # url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-    #{'document_root': settings.MEDIA_ROOT}
-    #    ),
-    url(r'^permit/create/$',
+   url(r'^permit/create/$',
         views.PermitCreate.as_view(), name="permit_create"),
     url(r'^permit/(?P<pk>\d+)/$',
         views.PermitDetail.as_view(), name="permit_detail"),
